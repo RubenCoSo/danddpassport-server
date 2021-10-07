1c0ac8f0d9dc4a65dc7a77b9110ede73a2f5e9a8
Ruben Corral <rubencorralsola@gmail.com>
2021-10-07 15:51:03 +0200

    master characters

--- routes/adventure.routes.js
+++ routes/adventure.routes.js
@@ -26,6 +26,41 @@ router.post("/newAdventure" , (req, res, next) => {
     .catch((err) => res.json(err));
 });
 
+router.put('/adventureCharacters',(req,res)=>{
+  const { adventureId, addedCharactersId} =req.body
+  Adventure.findById(adventureId)
+  .then((adventure)=>{
+   const filtChar = addedCharactersId.filter((character)=> !adventure.characters.includes(character))
+   console.log(filtChar);
+    Adventure.findByIdAndUpdate(adventureId,{$push:{characters: {$each:filtChar}}},{new:true})
+      .then((adventure)=>{
+        console.log(adventure);
+        //  res.json(adventure)
+        
+      })
+  
+    
+  })
+})
+
+
+router.get('/charactersInAdventure/:id',(req,res)=>{
+
+  const adventureId = req.params.id
+
+  Adventure.findById(adventureId)
+    .populate('characters')
+    .then((adventure)=>res.json(adventure))
+    .catch((err) => res.json(err));
+})
+
+router.get('/adventure/allCharacters',(req,res)=>{
+  Character.find({})
+  .then((characters)=>{
+    res.json(characters)
+  })
+})
+
 
 
 
